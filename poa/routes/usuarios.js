6cb77362a84f5543e362e36f8662e6605c903a55
Narciso <nar_ichi95@hotmail.com>
2019-01-25 10:13:30 -0600

    Upd & Opt

--- poa/routes/usuarios.js
+++ poa/routes/usuarios.js
@@ -1,28 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var cors = require('cors');
-
 router.use(cors());
-
-const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require("mongod").MongoClient;
 //crear bd de antes 
 const url = "mongodb://localhost:27017";
 const dbName ="Libreria";
-var ObjectId = require("mongodb").ObjectId;
-
-
-router.get('/', function(req,res,next)
-{
+var ObjectId = require("mongod").ObjectId;
+router.get('/', function(req,res,next){
     MongoClient.connect(url, function(err,client){
-
         if(err) throw err;
         console.log("Connected corretly to server");
         const db = client.db(dbName);
-
         var query ={};
-
-        db.collection("Persona").find(query).toArray(function(err,result)
-        {
+        db.collection("Persona").find(query).toArray(function(err,result){
             if(err) throw err;
             console.log(result);
            // res.render(result);
@@ -31,23 +22,14 @@ router.get('/', function(req,res,next)
         });
     });
 });
-
-
 //////////////////////////////////
-
-
-router.get('/getDatos', function(req,res,next)
-{
+router.get('/getDatos', function(req,res,next){
     MongoClient.connect(url, function(err,client){
-
         if(err) throw err;
         console.log("Connected corretly to server");
         const db = client.db(dbName);
-
         var query ={};
-
-        db.collection("alumnos").find(query).toArray(function(err,result)
-        {
+        db.collection("alumnos").find(query).toArray(function(err,result){
             if(err) throw err;
             console.log(result);
            // res.render(result);
@@ -56,160 +38,102 @@ router.get('/getDatos', function(req,res,next)
         });
     });
 });
-
-
-  
-  
-  
-
-  
 router.get('/alta', function(req, res, next) {
     res.render('formuser',{page:"usuarios",ope:"c"});
 });
-
-
 router.get('/modificar/:id', function(req, res, next) {
-
-	console.log(req.params.id);
-  var oId = new ObjectId(req.params.id);
-
-  var query = {"_id": oId};
-
-   MongoClient.connect(url, function(err,client){
-    if (err) throw err;
-    const db = client.db(dbName);
-    db.collection("Persona").findOne(query,function(err,result){
-        
-          var string=JSON.stringify(result);
-          console.log('>> string: ', string );
-          var json =  JSON.parse(string);
-
-          res.render('formuser',{usuarios:json,page:"usuarios",ope:"u"});
-          client.close();
-    });
-    
+    console.log(req.params.id);
+    var oId = new ObjectId(req.params.id);
+    var query = {"_id": oId};
+    MongoClient.connect(url, function(err,client){
+        if (err) throw err;
+        const db = client.db(dbName);
+        db.collection("Persona").findOne(query,function(err,result){
+            var string=JSON.stringify(result);
+            console.log('>> string: ', string );
+            var json =  JSON.parse(string);
+            res.render('formuser',{usuarios:json,page:"usuarios",ope:"u"});
+            client.close();
+        });
    });    
-
 });
-
 //alta,modificar,eliminar
-router.post('/operacion', function(req, res, next) {
-     
-      var tipoOperacion = "";
-      var nombre ="";
-      var apellido="";
-      var usuario="";
-      var rol="";
-      var correo="";
-      var contraseña="";
-
-      var query = "";
-
-      var id="";
-
-      
-      //console.log(req);
-      tipoOperacion = req.body.ope;
-
-     if (tipoOperacion == "c") {
-      
-      	//Ligar los datos con los campos
+router.post('/operacion', function(req, res, next) {  
+    var tipoOperacion = "";ç
+    var nombre ="";ç
+    var apellido="";
+    var usuario="";
+    var rol="";
+    var correo="";
+    var contraseña="";
+    var query = "";
+    var id="";
+    //console.log(req);ç
+    tipoOperacion = req.body.ope;
+    if (tipoOperacion == "c") {
+        //Ligar los datos con los campos
       	nombre = req.body.nombre;
       	apellido = req.body.apellido;
-      	 usuario = req.body.usuario;
+      	usuario = req.body.usuario;
         rol = req.body.rol;
         correo = req.body.correo;
         contraseña=req.body.contraseña;
-        
-      
-
         MongoClient.connect(url, function(err,client){
-        if (err) throw err;
-         const db = client.db(dbName);
-        var query =  {"Nombre":nombre,"Apellido":apellido,"Usuario":usuario,"Rol":rol,"Contraseña":contraseña,"Correo":correo};
-
-
-        db.collection("Persona").insertOne(query,function(err,result){
-            
+            if (err) throw err;
+            const db = client.db(dbName);
+            var query =  {"Nombre":nombre,"Apellido":apellido,"Usuario":usuario,"Rol":rol,"Contraseña":contraseña,"Correo":correo};
+            db.collection("Persona").insertOne(query,function(err,result){
             if (err) throw err;
             //console.log(result);
-
             if (parseInt(result.insertedCount.toString()) > 0) {
                 return res.send("1");
             }
-
             client.close();
         });
-        
        });
-
-        //query = "INSERT INTO usuarios (nombre,apellido,edad) VALUES('"+nombre+"','"+apellido+"',"+edad+");";
-
+       //query = "INSERT INTO usuarios (nombre,apellido,edad) VALUES('"+nombre+"','"+apellido+"',"+edad+");";
      }else if(tipoOperacion == "u"){
-     	  
         nombre = req.body.nombre;
       	apellido = req.body.apellido;
         usuario = req.body.usuario;
         rol = req.body.rol;
         correo = req.body.correo;
-        contraseña = req.body.contraseña;
-          
+        contraseña = req.body.contraseña;  
       	id = req.body.id;
-
         MongoClient.connect(url, function(err,client){
-        if (err) throw err;
-
-         var oId = new ObjectId(id);
-         var query = {"_id": oId};
-         var nuevosValores = {"Nombre":nombre,"Apellido":apellido,"Usuario":usuario,"Rol":rol,"Contraseña":contraseña,"Correo":correo};
-
-         console.log(nuevosValores);
-
-         const db = client.db(dbName);
-        db.collection("Persona").update(query,nuevosValores,function(err,resul){
-            
             if (err) throw err;
-            //console.log(result);
-            
-            if (parseInt(resul.result.nModified.toString()) > 0) {
-                return res.send("1");
-            }
-
+            var oId = new ObjectId(id);
+            var query = {"_id": oId};
+            var nuevosValores = {"Nombre":nombre,"Apellido":apellido,"Usuario":usuario,"Rol":rol,"Contraseña":contraseña,"Correo":correo};
+            console.log(nuevosValores);
+            const db = client.db(dbName);
+            db.collection("Persona").update(query,nuevosValores,function(err,resul){
+                if (err) throw err;
+                //console.log(result);
+                if (parseInt(resul.result.nModified.toString()) > 0) {
+                    return res.send("1");
+                }
             client.close();
         });
-        
-       });
-
-     //   query = "UPDATE usuarios SET nombre = '"+nombre+"', apellido = '"+apellido+"', edad="+edad+" WHERE id = "+id+" ";
-     }else if(tipoOperacion == "d"){
-
+    });
+    //   query = "UPDATE usuarios SET nombre = '"+nombre+"', apellido = '"+apellido+"', edad="+edad+" WHERE id = "+id+" ";
+    }else if(tipoOperacion == "d"){
      	id = req.body.id;
-      
-      MongoClient.connect(url, function(err,client){
-        if (err) throw err;
-
-         var oId = new ObjectId(id);
-         var query = {"_id": oId};
-         
-    const db = client.db(dbName);
-
-        db.collection("Persona").deleteOne(query,function(err,resul){
-            
+        MongoClient.connect(url, function(err,client){
             if (err) throw err;
-            
-            console.log(resul);
-            
-            if (parseInt(resul.deletedCount.toString()) > 0) {
-                return res.send("1");
-            }
-
+            var oId = new ObjectId(id);
+            var query = {"_id": oId};
+            const db = client.db(dbName);
+            db.collection("Persona").deleteOne(query,function(err,resul){
+                if (err) throw err;
+                console.log(resul);
+                if (parseInt(resul.deletedCount.toString()) > 0) {
+                    return res.send("1");
+                }
             client.close();
         });
-        
-       });
-
-      //  query = "DELETE FROM usuarios WHERE id = "+id+" ";
-     }
-
+    });
+    //  query = "DELETE FROM usuarios WHERE id = "+id+" ";
+    }
 });
 module.exports = router;
